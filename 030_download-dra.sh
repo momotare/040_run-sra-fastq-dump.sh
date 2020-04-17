c0ec4c8ce366ccf782de1d9e3dafa8cf1d01e426
momotare <62997434+momotare@users.noreply.github.com>
2020-04-17 17:51:32 +0900

    Update 030_download-dra.sh

--- 030_download-dra.sh
+++ 030_download-dra.sh
@@ -4,5 +4,6 @@ site="ftp://ftp.ddbj.nig.ac.jp"
 path1=ddbj_database/dra/sralite/ByExp/litesra
 path2=DRX/DRX005/DRX005949/DRR006760
 path3=DRR002191.sra
+
 curl -O ${site}/${path1}/${path2}/${path3}
 ls -lh ${path3}
